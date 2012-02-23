d400adc199de45332d32ad00558c6a8b631c6a21
torsten.witt <torsten.witt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-23 09:43:28 +0000

    Tabellenname geändert
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/libconnect/branches/libconnect3@58359 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Domain/Repository/subjectRepository.php
+++ Classes/Domain/Repository/subjectRepository.php
@@ -11,7 +11,7 @@ Class Tx_Libconnect_Domain_Repository_SubjectRepository extends Tx_Extbase_Persi
  		//$extbaseFrameworkConfiguration = Tx_Extbase_Dispatcher::getExtbaseFrameworkConfiguration();
 		//$pidList = implode(', ', t3lib_div::intExplode(',', $extbaseFrameworkConfiguration['persistence']['storagePid']));
 		$query = $this->createQuery();
-		$query->statement('SELECT * from tx_libconnect_subject');
+		$query->statement('SELECT * from tx_libconnect_domain_model_subject');
 		
 		return $query->execute();
  	}
