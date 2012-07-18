8c508aabab94a2ae7844469833b1a00638769278
torsten.witt <torsten.witt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-18 09:46:52 +0000

    DBIS: Im Suchformular werden nun die Umlaute für die Regionen richtig angezeigt
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/libconnect/tags@64670 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Release_3.2.1/Classes/Domain/Repository/SubjectRepository.php
@@ -0,0 +1,19 @@
+<?php
+Class Tx_Libconnect_Domain_Repository_SubjectRepository extends Tx_Extbase_Persistence_Repository {
+
+	public function findRandom() {
+        $rows = $this->createQuery()->execute()->count();
+        $row_number = mt_rand(0, max(0, ($rows - 1)));
+        return $this->createQuery()->setOffset($row_number)->setLimit(1)->execute();
+    }
+	
+	public function findAll() {
+ 		//$extbaseFrameworkConfiguration = Tx_Extbase_Dispatcher::getExtbaseFrameworkConfiguration();
+		//$pidList = implode(', ', t3lib_div::intExplode(',', $extbaseFrameworkConfiguration['persistence']['storagePid']));
+		$query = $this->createQuery();
+		$query->statement('SELECT * from tx_libconnect_domain_model_subject');
+		
+		return $query->execute();
+ 	}
+}
+?>
