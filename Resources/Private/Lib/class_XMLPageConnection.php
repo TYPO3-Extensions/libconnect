bd21f0fdc9280f64a082218694c8f0c89221ff5b
bababil <bababil@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-08-08 09:21:17 +0000

    DBIS und EZB Umstellung von simplexml_load_file auf CURL. Dabei wurde die CURL Verbindung und das erzeugen des XML Objektes in eine eigene Klasse ausgelagert damit diese bei neuen Bibliothek-Schnittstellen verwendet werden kann.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/libconnect/trunk@65264 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Resources/Private/Lib/class_XMLPageConnection.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Class 'XMLPageConnection' for the 'libconnect' extension.
+ *
+ * @author	Björn Heinermann <hein@zhaw.ch>
+ * @package	TYPO3
+ * @subpackage	tx_libconnect
+ */
+
+class XMLPageConnection {
+    
+    private $extPiVars;
+    private $proxy;
+    private $proxyPort;
+    
+    public function __construct() {
+	
+	$this->setExtPiVars();
+	$this->setProxy();
+	$this->setProxyPort();
+    }
+    
+    /**
+     * Läd die im typoscript gesetzten Variablen.
+     */
+    private function setExtPiVars() {
+	$this->extPiVars = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_libconnect.'];
+    }
+    
+    /**
+     * Setzt den Proxy Server 
+     */
+    private function setProxy() {
+	$this->proxy = $this->extPiVars['proxy'];
+    }
+    
+    /**
+     * Setzt den Proxy Port 
+     */
+    private function setProxyPort() {
+	$this->proxyPort = $this->extPiVars['proxy_port'];
+    }
+    
+    /**
+     * Holt XML Daten von einer Internetseite und gibt diese als array zurück
+     *
+     * @return SimpleXMLElement object
+     */
+    public function getDataFromXMLPage($url) {
+
+	$xmlObj = FALSE;
+	$ch = curl_init();
+	 
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_PORT, 80);
+	if ($this->proxy && $this->proxyPort) {
+	    curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
+	    curl_setopt($ch, CURLOPT_PROXYPORT, $this->proxyPort);
+	}
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+	
+	$result = curl_exec($ch);
+	$xmlObj = simplexml_load_string($result);
+	
+	/* HINWEIS FEHLERPRUEFUNG: 
+	 * Die Funktion curl_error() wie auch curl_getinfo() haben keine brauchbaren Rückgabewerte
+	 * zurückgegeben. Deswegen die Pruefung ob später ein Object erzeugt wurde und es ein
+	 * SimpleXMLElement ist. 
+	 */
+	if (!is_object($xmlObj) && get_class($xmlObj) == 'SimpleXMLElement') {
+	    $xmlObj = FALSE;
+	}
+
+	return $xmlObj;
+    }
+}
+?>
