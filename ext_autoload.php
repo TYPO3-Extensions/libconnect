cb321482acf8699fa5dd59ba073aa37da856a24f
torsten.witt <torsten.witt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-15 14:08:53 +0000

    dient zum Laden von Klassen
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/libconnect/trunk@67867 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_autoload.php
@@ -0,0 +1,7 @@
+<?php
+$extensionPath = t3lib_extMgm::extPath('libconnect');
+
+return array(
+    'user_libconnect_hasselectedpluginforcssinclude' => $extensionPath . '/Classes/userFunctions/user_libconnect_hasSelectedPluginForCSSInclude.php'
+);
+?>
