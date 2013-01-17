e314b1b7172d7d40b0d305a42105c9e3ad7a888c
torsten.witt <torsten.witt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-17 08:25:25 +0000

    erstmal raus
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/libconnect/trunk@70112 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-$extensionPath = t3lib_extMgm::extPath('libconnect');
-
-return array(
-    'user_libconnect_hasselectedpluginforcssinclude' => $extensionPath . '/Classes/userFunctions/user_libconnect_hasSelectedPluginForCSSInclude.php'
-);
-?>
