55c6ca53c207a8b7b23509ac3a7d8706b50bb1e6
torsten.witt <torsten.witt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-10-22 06:53:07 +0000

    Einrückung angepasst
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/libconnect/trunk@86727 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/UserFunctions/user_libconnect_hasSelectedPluginForCSSInclude.php
+++ Classes/UserFunctions/user_libconnect_hasSelectedPluginForCSSInclude.php
@@ -16,20 +16,20 @@
  */
 function user_libconnect_hasSelectedPluginForCSSInclude($type) {
 
-	    $pid = $GLOBALS['TSFE']->id;
-	    $list_type = 'libconnect_'.$type;
+    $pid = $GLOBALS['TSFE']->id;
+    $list_type = 'libconnect_'.$type;
 
-	    $select = 'uid';
-	    $from = 'tt_content';
-	    $where = 'pid = "'.$pid.'" AND list_type = "'.$GLOBALS['TYPO3_DB']->quoteStr($list_type, 'tt_content').'" AND deleted = "0"';
-	    $groupBy = '';
-	    $orderBy = '';
-	    $limit = '';
-	    $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select, $from, $where, $groupBy, $orderBy, $limit);
+    $select = 'uid';
+    $from = 'tt_content';
+    $where = 'pid = "'.$pid.'" AND list_type = "'.$GLOBALS['TYPO3_DB']->quoteStr($list_type, 'tt_content').'" AND deleted = "0"';
+    $groupBy = '';
+    $orderBy = '';
+    $limit = '';
+    $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select, $from, $where, $groupBy, $orderBy, $limit);
 
-	    if ($GLOBALS['TYPO3_DB']->sql_num_rows($res)) {
-			return TRUE;
-		}
+    if ($GLOBALS['TYPO3_DB']->sql_num_rows($res)) {
+        return TRUE;
+    }
 
     return FALSE;
 }
