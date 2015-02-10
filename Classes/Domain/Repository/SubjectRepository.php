9656ef10e846d6b2e0beb4f3a6d2df575207566f
torsten.witt <torsten.witt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-02-10 13:50:52 +0000

    nicht benötigten Code entfernt, den "Smooth Migration" angemerkt hat
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/libconnect/trunk@88353 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Domain/Repository/SubjectRepository.php
+++ Classes/Domain/Repository/SubjectRepository.php
@@ -51,7 +51,6 @@ Class Tx_Libconnect_Domain_Repository_SubjectRepository extends Tx_Extbase_Persi
      * @return object
      */
     public function findAll() {
-        //$extbaseFrameworkConfiguration = Tx_Extbase_Dispatcher::getExtbaseFrameworkConfiguration();
         //$pidList = implode(', ', t3lib_div::intExplode(',', $extbaseFrameworkConfiguration['persistence']['storagePid']));
         $query = $this->createQuery();
         $query->statement('SELECT * from tx_libconnect_domain_model_subject');
